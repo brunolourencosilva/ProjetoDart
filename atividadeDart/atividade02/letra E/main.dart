fcc2a7781136cf74455c8e4c184810c4982ace98
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-10-23 19:15:08 -0300

    terminar

--- atividadeDart/atividade02/letra E/main.dart
+++ atividadeDart/atividade02/letra E/main.dart
@@ -11,14 +11,16 @@ void main(){
 
     if(valor1 == null || valor1.isEmpty){
       print('Valor invalido!');
-    }else{
-      int? temp1 = int.tryParse(valor1);
-      if(temp1 == null || temp1 <= 0){
-        print('Valor invalido!');
-      }else{
-        var segundos = temp1;
-      }
+      continue;
     }
+
+    int? temp1 = int.tryParse(valor1);
+    if(temp1 == null || temp1 <= 0){
+      print('Valor invalido!');
+      continue;
+    }
+
+    var segundos = temp1;
 
     // ===== Segundo entrada =====
     stdout.write('Informe uma aceleração: ');
@@ -26,16 +28,34 @@ void main(){
 
     if(valor2 == null || valor2.isEmpty){
       print('Valor invalido!');
-    }else{
-      int? temp2 = int.tryParse(valor2);
-      if(temp2 == null || temp2 <= 0){
-        print('Valor invalido!');
-      }else{
-        // var aceleracao = temp2;
-      }
+      continue;
     }
 
-    // for(){}
+    int? temp2 = int.tryParse(valor2);
+    if(temp2 == null || temp2 <= 0){
+      print('Valor invalido!');
+      continue;
+    }
 
+    var aceleracao = temp2;
+
+    for (int i = 1; i <= segundos; i++) {
+      int velocidade = aceleracao * i;
+      print('Velocidade no segundo $i: $velocidade m/s');
+    }
+
+    while(true){
+      stdout.write('Deseja continuar? (SIM/NAO)');
+      String? escolha = stdin.readLineSync()?.toUpperCase();
+
+      if(escolha == null){
+        print('Valor invalidos!');
+        continue;
+      }
+
+      if(escolha == "SIM"){
+        print('Continuando programa...');
+      }
+    }
   }
 }
