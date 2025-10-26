b2d6c7eae9ef7075b526e2747ad05c855ffaccc1
brunolourencosilva <brunolourenco.silva15@gmail.com>
2025-10-26 17:46:10 -0300

    feito(talvez certio)

--- atividadeDart/atividade02/letra H/main.dart
+++ atividadeDart/atividade02/letra H/main.dart
@@ -4,36 +4,52 @@
 import 'dart:io';
 
 void main(){
-  print('='*70);
-  print('Cálculo de velocidade média');
-  print('='*70);
-
-  while(true){
+    print('='*70);
+    print('Cálculo de velocidade média');
+    print('='*70);
+while(true){
     print('');
 
-    stdout.write('Informe um valor de expoente: ');
-    String? valor1 = stdin.readLineSync();
+    double distanciaTotalKm = 0;
+    double tempoTotalHoras = 0;
 
-    if(valor1 == null || valor1.isEmpty){
-      print('Valor invalido!');
-      continue;
+    for (int i = 1; i <= 4; i++) {
+      
+      // Distância
+      stdout.write('Informe a distância percorrida no trecho $i (em metros): ');
+      String? entradaDist = stdin.readLineSync();
+      double? distancia = double.tryParse(entradaDist ?? '');
+      if (distancia == null || distancia <= 0) {
+        print('Valor inválido!');
+        i--;
+        continue;
+      }
+
+      // Tempo
+      stdout.write('Informe o tempo gasto no trecho $i (em minutos): ');
+      String? entradaTempo = stdin.readLineSync();
+      double? tempo = double.tryParse(entradaTempo ?? '');
+      if (tempo == null || tempo <= 0) {
+        print('Valor inválido!');
+        i--;
+        continue;
+      }
+      print('');
+
+      // Converte e acumula
+      distanciaTotalKm += distancia / 1000; // metros → km
+      tempoTotalHoras += tempo / 60;        // minutos → horas
     }
 
-    int? temp1 = int.tryParse(valor1);
-    if(temp1 == null || temp1 <= 0){
-      print('Valor invalido!');
-      continue;
-    }
 
-    int? expoente = temp1;
-
-    print('-'*70);
-    for (int i = 0; i <= expoente ; i++) {
-      var potencia = pow(2,i);
-      print('2 ^ $i = $potencia');
-    }
+    double velocidadeMediaKmH = distanciaTotalKm / tempoTotalHoras;
+    double velocidadeMediaMph = (distanciaTotalKm * 1000) / tempoTotalHoras;
 
     print('');
+    print('Velocidade média total: ${velocidadeMediaKmH.toStringAsFixed(2)} km/h');
+    print('Velocidade média total: ${velocidadeMediaMph.toStringAsFixed(2)} metros por hora');
+    print('');
+
     print('.'*70);
     while(true){
       stdout.write('Deseja continuar? (SIM/NAO)');
