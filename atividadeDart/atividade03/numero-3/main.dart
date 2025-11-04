8abd47b7c3c57578b26b7397dbe36a621f962ad9
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-03 21:36:19 -0300

    terminar

--- atividadeDart/atividade03/numero-3/main.dart
+++ atividadeDart/atividade03/numero-3/main.dart
@@ -1,3 +1,13 @@
 //  3. Cálculo de Média Ponderada Após Descarte
 //  Considere uma lista de notas de alunos. O exercício exige que se descarte a menor nota da lista e, 
 //  em seguida, calcule a média aritmética das notas restantes. O resultado deve ser um número decimal (double).
+
+void main(){
+  List<List<dynamic>> alunos = [
+    ['Okuyasu', 8.5, 9.0, 7.5],
+    ['Josuke', 6.0, 7.5, 8.0],
+    ['Koiche', 9.0, 8.5, 10.0],
+  ];
+
+  List<double> descartaMenorNota = 
+}
