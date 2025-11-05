3fd222a1da623dfb29a669e41976c970265148f5
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-05 20:24:22 -0300

    organização

--- atividadeDart/atividade03/numero-9/main.dart
+++ atividadeDart/atividade03/numero-9/main.dart
@@ -1,40 +1,53 @@
-//  10. Reversão Manual da Ordem dos Elementos
-//  Dada uma lista de strings, crie uma nova lista que contenha os mesmos elementos, 
-//  mas na ordem inversa da lista original. 
-//  O uso de List.reversed é permitido para fins de comparação, 
-//  mas a implementação principal deve usar uma estrutura de repetição (for ou while).
+//  Filtragem de Emails Válidos (Regra Simples)
+//  Dada uma lista de strings que supostamente são endereços de email,
+//  filtre e colete em uma nova lista apenas os emails que contêm exatamente um caractere '@' e terminam com ".com".
 
-import 'dart:io';
 
 void main(){
 
-  List<String> palavras = [
-    'Imperium',
-    'Astartes',
-    'Warp',
-    'Heresy',
-    'Daemon',
-    'Terra',
-    'Inquisition',
-    'Exterminatus',
-    'Primarch',
-    'Adeptus'
+  List<String> emails = [
+    // válidos
+    'arthur.morgan@gmail.com',
+    'sadie.adler@gmail.com',
+    'john.marston@gmail.com',
+    'dutch.vanderlinde@yahoo.com',
+    'charles.smith@yahoo.com',
+
+    // inválidos
+    'micah.bell@@outlaws..com',
+    'reverend@swanson@faith',
+    'hosea-matthews.mail.com',
+    'uncle@lazyhorse',
+    'jack_marston@.prairie',
   ];
 
-  List<String> listaInversa = [];
+  List<String> emailsValidos = [];
 
-  for (int i = palavras.length - 1; i >= 0;i--){
-    listaInversa.add(palavras[i]);
+  for (var i in emails) {
+    if (i.split('@').length == 2 && i.contains('.com') ){
+      emailsValidos.add(i);
+    }
   }
+   print('/'*70);
+  print('');
+  print('Filtragem de Emails Válidos (Regra Simples)');
+  print('');
 
-  print('/' * 70);
+  print('-'*70);
   print('');
-  print('-' * 70);
-  print('Reversão Manual da Ordem dos Elementos');
-  print('-' * 70);
+  print('Lista original:');
   print('');
-  print('Lista original: $palavras');
-  print('Lista invertida: $listaInversa');
+  for (var i = 0; i < emails.length; i++){
+    print(emails[i]);
+    print('');
+  }
+  print('-'*70);
   print('');
-  print('/' * 70);
+  print('Emails validos:');
+  print('');
+  for (var i = 0; i < emailsValidos.length; i++){
+    print(emailsValidos[i]);
+    print('');
+  }
+   print('/'*70);
 }
