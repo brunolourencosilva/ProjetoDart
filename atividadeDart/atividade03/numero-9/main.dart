97b98a32fce108f0658851cdcd7e08deb3ec80da
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-04 21:33:40 -0300

    terminar

---
+++ atividadeDart/atividade03/numero-9/main.dart
@@ -0,0 +1,3 @@
+//  Filtragem de Emails Válidos (Regra Simples)
+//  Dada uma lista de strings que supostamente são endereços de email,
+//  filtre e colete em uma nova lista apenas os emails que contêm exatamente um caractere '@' e terminam com ".com".
