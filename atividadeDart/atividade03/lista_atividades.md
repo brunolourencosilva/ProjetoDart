c25adc630c11c08f981eb0a02f26b9bd12582b1a
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-04 21:11:20 -0300

    feito

--- atividadeDart/atividade03/lista_atividades.md
+++ atividadeDart/atividade03/lista_atividades.md
@@ -31,7 +31,7 @@ Obrigatório:
         2 = a palavra deve começar com a letra 'A' (maiúscula ou minúscula).
 
 
-- [ ] 7. Criação de Frase com Filtro
+- [✅] 7. Criação de Frase com Filtro
         Dada uma lista de strings, crie uma única string (frase) concatenando apenas as palavras que terminam com a letra 'R' (maiúscula ou minúscula). As palavras devem ser separadas por um espaço na frase final.
  
 
---
+++ atividadeDart/atividade03/numero-7/main.dart
@@ -0,0 +1,40 @@
+//  7. Criação de Frase com Filtro
+//  Dada uma lista de strings, crie uma única string (frase) concatenando apenas as 
+//  palavras que terminam com a letra 'R' (maiúscula ou minúscula). As palavras devem ser separadas por um espaço na frase final.
+
+void main() {
+  List<String> palavras = [
+    "Abyss",
+    "Hollow",
+    "Ember",
+    "Lamentar",
+    "Crepúsculo",
+    "Ceniza",
+    "Eclipse",
+    "Silenciar",
+    "Ocaso",
+    "Perdurar"
+  ];
+
+  List<String> listaFiltrada = [];
+  
+  for (var i in palavras) {
+    if ((i.endsWith('R')) || i.endsWith('r')) {
+      listaFiltrada.add(i);
+    }
+  }
+
+  String fraseFinal = listaFiltrada.join(' ');
+
+  print('/' * 70);
+  print('');
+  print('-' * 70);
+  print('Filtragem Dupla em Lista de Palavras');
+  print('-' * 70);
+  print('');
+  print('Lista original: $palavras');
+  print('Frase final: $fraseFinal');
+  print('');
+  print('/' * 70);
+}
+
