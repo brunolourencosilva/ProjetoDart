7d95c4ccffbc4f4ad682df24ea2bdbf23375e0a7
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-25 19:58:37 -0300

    feito

--- atividadeDart/atividade05/numero-3/main.dart
+++ atividadeDart/atividade05/numero-3/main.dart
@@ -1,21 +1,12 @@
-//  3. Cálculo de Média Ponderada Após Descarte
+//  3. Cálculo de Média Ponderada Após Descarte com função
 //  Considere uma lista de notas de alunos. O exercício exige que se descarte a menor nota da lista e, 
 //  em seguida, calcule a média aritmética das notas restantes. O resultado deve ser um número decimal (double).
 
-void main() {
-  List<List<dynamic>> alunos = [
-    ['Okuyasu', 4.5, 6.0, 5.5],
-    ['Josuke', 6.0, 7.5, 8.0],
-    ['Koiche', 9.0, 8.5, 10.0],
-  ];
+ void calcularMedia(List<List<dynamic>> lista) {
+  Set<int> valoresNaoUnicos = {};
+  Set<int> duplicados = {};
 
-  print('/'*70);
-  print('');
-  print('-'*70);
-  print('Cálculo de Média Ponderada Após Descarte');
-  print('-'*70);
-
-  for (var aluno in alunos) {
+  for (var aluno in lista) {
     String nome = aluno[0];
     List<double> notas = aluno.sublist(1).cast<double>();
 
@@ -25,6 +16,22 @@ void main() {
 
     print('Aluno: $nome | Média (menor nota descartada): ${media.toStringAsFixed(2)}');
   }
+}
+
+void main() {
+  List<List<dynamic>> alunos = [
+    ['Okuyasu', 4.5, 6.0, 5.5],
+    ['Josuke', 6.0, 7.5, 8.0],
+    ['Koiche', 9.0, 8.5, 10.0],
+  ];
+
+
+  print('/'*70);
+  print('');
+  print('-'*70);
+  print('Cálculo de Média Ponderada Após Descarte');
+  print('-'*70);
+  calcularMedia(alunos);
   print('');
   print('/'*70);
 }
