e70089d4109d3b29069671769eb6540b33940d07
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-25 20:38:22 -0300

    feito

--- atividadeDart/atividade05/numero-4/main.dart
+++ atividadeDart/atividade05/numero-4/main.dart
@@ -1,30 +1,49 @@
-//  4. Multiplicação de Elementos Ímpares
+//  4. Multiplicação de Elementos Ímpares com função
 //  Percorra uma lista de números inteiros. Para cada número, verifique se ele é ímpar. 
 //  Se for ímpar, ele deve ser multiplicado por 2. Se for par, ele deve permanecer inalterado.
 //  O resultado final deve ser uma nova lista com os valores transformados.
 
-void main(){
-  List<int> listaInteiros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+Map<String, List<int>> filtroImparPar(List<int> lista) {
   List<int> listaPar = [];
   List<int> listaImpar = [];
 
-  for (int i in listaInteiros){
-    if (i % 2 == 0){
+  for (int i in lista) {
+    if (i % 2 == 0) {
       listaPar.add(i);
-    } else{
-      listaImpar.add(i*2);
+    } else {
+      listaImpar.add(i * 2);
     }
   }
 
-  print('/'*70);
+  return {
+    'pares': listaPar,
+    'impares': listaImpar,
+  };
+}
+
+void main() {
+  List<int> listaInteiros = [
+    1,2,3,4,5,6,7,8,9,10,
+    11,12,13,14,15,16,17,18,19,20
+  ];
+
+  var resultado = filtroImparPar(listaInteiros);
+
+  print('/' * 70);
   print('');
-  print('-'*70);
+  print('-' * 70);
   print('Multiplicação de Elementos Ímpares');
-  print('-'*70);
+  print('-' * 70);
   print('');
-  print('Lista de numeros impares:\n$listaImpar');
-  print('Lista de numeros pares:\n$listaPar');
+
+  print('Lista de números ímpares multiplicados:');
+  print(resultado['impares']);
+
   print('');
-  print('/'*70);
+  print('Lista de números pares:');
+  print(resultado['pares']);
+
+  print('');
+  print('/' * 70);
 }
 
