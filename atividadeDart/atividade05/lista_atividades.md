1ee265f1e2766db4736ea2b28f6bbdb09e897c67
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-25 19:32:22 -0300

    feito

--- atividadeDart/atividade05/lista_atividades.md
+++ atividadeDart/atividade05/lista_atividades.md
@@ -5,7 +5,7 @@ Refatoração da lista de atividade03 porem com função;
 
 ### ✅ Checklist de Exercícios
 
-- [ ] 1 -  Filtragem e Soma Condicional de Pares:
+- [✅] 1 -  Filtragem e Soma Condicional de Pares:
         Dada uma lista de números inteiros, o objetivo é primeiro filtrar apenas os números que são pares e, simultaneamente, maiores que 10. Após a filtragem, deve-se calcular a soma total desses números restantes.
 
 - [ ] 2. Identificação e Contagem de Ocorrências Distintas:
--- atividadeDart/atividade05/numero-1/main.dart
+++ atividadeDart/atividade05/numero-1/main.dart
@@ -1,26 +1,22 @@
-//  1 -  Filtragem e Soma Condicional de Pares:
+//  1 -  Filtragem e Soma Condicional de Pares com função:
 //  Dada uma lista de números inteiros, o objetivo é primeiro filtrar apenas os números que são pares e, simultaneamente, maiores que 10.
 //  Após a filtragem, deve-se calcular a soma total desses números restantes.
 
-void main(){
-
-  List<int> listaInteiros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
-  List<int> numerosPares = listaInteiros.where((num) => num % 2 == 0).toList();
-  List<int> numerosMaior10 = numerosPares.where((num) => num >= 10 ).toList();
+int somaParesMaiorQue10(List<int> lista) {
+  List<int> filtrados = lista.where((n) => n % 2 == 0 && n >= 10).toList();
 
   int soma = 0;
-
-  for (int n in numerosMaior10){
+  for (int n in filtrados) {
     soma += n;
   }
 
-  print('/'*70);
-  print('');
-  print('Filtragem e Soma Condicional de Pares:');
-  print('');
-  print('Lista dos numeros inteiros: $listaInteiros');
-  print('lista dos numeros pares: $numerosPares');
-  print('Soma total dos números restantes: $soma');
-  print('');
-  print('/'*70);
+  return soma;
+}
+
+void main() {
+  List<int> listaInteiros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+  int resultado = somaParesMaiorQue10(listaInteiros);
+
+  print('Soma dos pares >= 10: $resultado');
 }
