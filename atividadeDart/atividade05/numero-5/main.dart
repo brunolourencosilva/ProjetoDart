89489114c7125e9f69282235066e4efd5962be3a
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-11-26 20:52:28 -0300

    feito

--- atividadeDart/atividade05/numero-5/main.dart
+++ atividadeDart/atividade05/numero-5/main.dart
@@ -2,18 +2,37 @@
 //  Dada uma lista de números inteiros, encontre e imprima o segundo maior valor presente na lista.
 //  A lista pode conter duplicatas, mas o segundo maior valor deve ser o valor distinto que é imediatamente menor que o maior valor.
 
-void main(){
-  List<int> listaInteiros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-  int penultimo = listaInteiros[listaInteiros.length - 2];
+int? filtroPenultimoNumero({
+  required List<int> lista,
+}) {
+  if (lista.length < 2) {
+    print("Lista possui menos de 2 elementos.");
+    return null;
+  }
 
-  print('/'*70);
+  int penultimo = lista[lista.length - 2];
+
+  print("Penúltimo número: $penultimo");
+
+  return penultimo;
+}
+
+void main() {
+  List<int> listaInteiros = [
+    1,2,3,4,5,6,7,8,9,10,
+    11,12,13,14,15,16,17,18,19,20
+  ];
+
+  int? penultimo = filtroPenultimoNumero(lista: listaInteiros);
+
+  print('/' * 70);
   print('');
-  print('-'*70);
+  print('-' * 70);
   print('Determinação do Segundo Valor de Pico');
-  print('-'*70);
+  print('-' * 70);
   print('');
-  print('Penultimo numero: $penultimo');
+  print('Penúltimo número: $penultimo');
   print('');
-  print('/'*70);
+  print('/' * 70);
 }
 
